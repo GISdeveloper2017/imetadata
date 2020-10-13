0c5dca55f763be0d1f5cb1a2c20184fdd57215d2
zhaoyf08 <zhaoyf08@163.com>
2020-10-13 17:31:23 +0800

    TODO 王学谦 对字段值根据字段类型进行检验

--- imetadata/business/metadata/base/parser/metadata/quality/c_audit.py
+++ imetadata/business/metadata/base/parser/metadata/quality/c_audit.py
@@ -115,7 +115,7 @@ class CAudit(CResource):
         :param result_template: 检查结果的模板
         :param value: 待检验的值, 可能为None
         :param title_prefix:
-        :param value_type: 类型（整数类型、小数类型、文本型、日期类型）
+        :param value_type: 类型（正整数类型、数字类型（科学计数法/正负整数/正负小数）、文本类型、日期类型...）
         :return:
         """
         pass
