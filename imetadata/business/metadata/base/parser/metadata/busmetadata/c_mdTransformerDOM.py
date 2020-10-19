da114e4146f19c4788156be9631f9c784533a63f
wxy <wangxiya@me.com>
2020-10-19 11:03:46 +0800

    更新DOM元数据转换模式
    Signed-off-by: wxy <wangxiya@me.com>

--- imetadata/business/metadata/base/parser/metadata/busmetadata/c_mdTransformerDOM.py
+++ imetadata/business/metadata/base/parser/metadata/busmetadata/c_mdTransformerDOM.py
@@ -2,7 +2,10 @@
 # @Time : 2020/10/17 13:31 
 # @Author : 王西亚 
 # @File : c_mdTransformerDOM.py
+
+from imetadata.base.c_result import CResult
 from imetadata.base.c_utils import CUtils
+from imetadata.base.c_xml import CXml
 from imetadata.business.metadata.base.parser.metadata.busmetadata.c_mdTransformer import CMDTransformer
 import pypyodbc
 
@@ -14,15 +17,48 @@ class CMDTransformerDOM(CMDTransformer):
         """
         super().process()
 
-        if CUtils.equal_ignore_case(self.transformer_type, self.Transformer_DOM_MDB):
-            return self.__process_file_format__(self.MetaDataFormat_XML)
+        xml_obj = CXml()
+        root_element = xml_obj.new_xml('root')
+        xml_obj.set_attr(root_element, self.Name_Type, self.transformer_type)
+        try:
+            if CUtils.equal_ignore_case(self.transformer_type, self.Transformer_DOM_MDB):
+                self.mdb_to_xml(self.object_name, self.file_info.__file_name_with_full_path__, root_element)
+            elif CUtils.equal_ignore_case(self.transformer_type, self.Transformer_DOM_MAT):
+                pass
+            elif CUtils.equal_ignore_case(self.transformer_type, self.Transformer_DOM_XLS):
+                pass
+            elif CUtils.equal_ignore_case(self.transformer_type, self.Transformer_DOM_XLSX):
+                pass
 
-    def mdb_to_xml(self, file_main_name, file_with_path, xml_obj):
+            super().metadata.set_metadata_bus(
+                self.Success,
+                '元数据文件[{0}]成功加载! '.format(self.file_info.__file_name_with_full_path__),
+                self.MetaDataFormat_XML,
+                xml_obj.to_xml()
+            )
+
+            return CResult.merge_result(
+                self.Success,
+                '元数据文件[{0}]成功加载! '.format(self.file_info.__file_name_with_full_path__)
+            )
+        except:
+            super().metadata.set_metadata_bus(
+                self.Exception,
+                '元数据文件[{0}]格式不合法或解析异常, 无法处理! '.format(self.file_info.__file_name_with_full_path__),
+                self.MetaDataFormat_Text,
+                ''
+            )
+            return CResult.merge_result(
+                self.Exception,
+                '元数据文件[{0}]格式不合法或解析异常, 无法处理! '.format(self.file_info.__file_name_with_full_path__)
+            )
+
+    def mdb_to_xml(self, file_main_name, file_with_path, parent_element):
         """
          TODO 王学谦 mdb文件转xml文件 by王学谦 初版，需要改进与调试
         :param file_main_name:文件主名，不带后缀名，同时也是mdb的表名
         :param file_with_path:文件全名，带路径
-        :param xml_obj:进行转换的xml对象，一半在函数外提前定义
+        :param parent_element:进行转换的xml对象，一半在函数外提前定义
         :return: xml_obj:转换好的xml对象
         """
         tablename = file_main_name  # 暂时 表名为与文件名一样 by王学谦
@@ -36,14 +72,13 @@ class CMDTransformerDOM(CMDTransformer):
         alldata = cur.fetchall()
         # total_rows = len(alldata)  # 行
         # total_cols = len(alldata[0])  # 列
-        node_property = xml_obj.create_element(xml_obj.xpath_one('/root'), 'property')
-        xml_obj.set_attr(node_property, 'tablename', tablename)
+        node_property = CXml.create_element(parent_element, 'property')
+        CXml.set_attr(node_property, 'tablename', tablename)
         for field_index, row_obj in enumerate(cur.description):
             if CUtils.equal_ignore_case(row_obj[0], 'shape'):  # 跳过shape字段
                 continue
-            node_item = xml_obj.create_element(node_property, 'item')
-            xml_obj.set_attr(node_item, 'name', row_obj[0])
-            xml_obj.set_element_text(node_item, alldata[0][field_index])
+            node_item = parent_element.create_element(node_property, 'item')
+            parent_element.set_attr(node_item, 'name', row_obj[0])
+            parent_element.set_element_text(node_item, alldata[0][field_index])
         cur.close()
         conn.close()
-        return xml_obj
