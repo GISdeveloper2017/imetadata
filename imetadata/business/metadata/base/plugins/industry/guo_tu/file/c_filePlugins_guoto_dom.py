0865f73ed908dff2eeb602cc8f1861ec31bdbfa9
zhaoyf08 <zhaoyf08@163.com>
2020-10-28 20:04:39 +0800

    插件修改与开发（DEM/DOM，GF1-abc卫星）（李宪、邢凯）

--- imetadata/business/metadata/base/plugins/industry/guo_tu/file/c_filePlugins_guoto_dom.py
+++ imetadata/business/metadata/base/plugins/industry/guo_tu/file/c_filePlugins_guoto_dom.py
@@ -244,7 +244,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'sjbqdwm',
                 self.Name_Title: '单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -255,7 +255,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'sjscdwm',
                 self.Name_Title: '单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -266,7 +266,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'sjcbdwm',
                 self.Name_Title: '单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -287,7 +287,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'mj',
                 self.Name_Title: '密级',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -298,7 +298,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'th',
                 self.Name_Title: '图号',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -309,7 +309,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dmfbl',
                 self.Name_Title: '地面分辨率',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -320,7 +320,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'yxscms',
                 self.Name_Title: '影像色彩模式',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -331,7 +331,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'xsws',
                 self.Name_Title: '影像位深',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -353,7 +353,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dtty',
                 self.Name_Title: '地图投影',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -364,7 +364,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'wxmc',
                 self.Name_Title: '卫星名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -375,7 +375,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'qsyxcgqlx',
                 self.Name_Title: '全色影像传感器',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -386,7 +386,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'qswxyxfbl',
                 self.Name_Title: '全色卫星影像分辨率',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -397,7 +397,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'qswxyxgdh',
                 self.Name_Title: '全色卫星影像轨道号',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -407,7 +407,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'qswxyxhqsj',
                 self.Name_Title: '全色卫星影像获取时间',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -418,7 +418,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dgpyxcgqlx',
                 self.Name_Title: '多光谱影像传感器',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -429,7 +429,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dgpwxyxfbl',
                 self.Name_Title: '多光谱卫星影像分辨率',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -440,7 +440,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dgpwxyxgdh',
                 self.Name_Title: '多光谱卫星影像轨道号',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -450,7 +450,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: 'dgpwxyxhqsj',
                 self.Name_Title: '多光谱卫星影像获取时间',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             }
         ]
 
@@ -522,7 +522,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '像素位数',
                 self.Name_Title: '像素位数',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -533,7 +533,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '地图投影',
                 self.Name_Title: '地图投影',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -544,7 +544,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '卫星名称',
                 self.Name_Title: '卫星名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -555,7 +555,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '数据格式',
                 self.Name_Title: '数据格式',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -566,7 +566,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '数据生产单位名',
                 self.Name_Title: '数据生产单位名',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -577,7 +577,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '数据版权单位名',
                 self.Name_Title: '数据版权单位名',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -588,7 +588,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '数据出版单位名',
                 self.Name_Title: '数据出版单位名',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             }
         ]
 
@@ -660,7 +660,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '像素位数',
                 self.Name_Title: '像素位数',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -671,7 +671,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '地图投影名称',
                 self.Name_Title: '地图投影名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -682,7 +682,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '卫星名称',
                 self.Name_Title: '卫星名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -693,7 +693,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '数据格式',
                 self.Name_Title: '数据格式',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -704,7 +704,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '产品生产单位名称',
                 self.Name_Title: '产品生产单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -715,7 +715,7 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '产品所有权单位名称',
                 self.Name_Title: '产品所有权单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             },
             {
                 self.Name_Type: self.QA_Type_XML_Node_Exist,
@@ -726,6 +726,6 @@ class CFilePlugins_GUOTU_DOM(CFilePlugins_GUOTU):
                 self.Name_ID: '产品出版单位名称',
                 self.Name_Title: '产品出版单位名称',
                 self.Name_Group: self.QA_Group_Data_Integrity,
-                self.Name_Result: self.QA_Result_Warn
+                self.Name_Result: self.QA_Result_Error
             }
         ]
