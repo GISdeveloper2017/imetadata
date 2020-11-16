392530be5df563a564e9c8333c7d8644269d1453
wxy <wangxiya@me.com>
2020-11-16 18:00:31 +0800

    修改元数据存储的逻辑, 分析质检结果有误的问题.
    Signed-off-by: wxy <wangxiya@me.com>

--- imetadata/business/metadata/base/parser/metadata/c_metaDataParser.py
+++ imetadata/business/metadata/base/parser/metadata/c_metaDataParser.py
@@ -13,7 +13,6 @@ from imetadata.business.metadata.base.parser.metadata.metadata.c_mdExtractorMng 
 from imetadata.business.metadata.base.parser.metadata.quality.c_audit import CAudit
 from imetadata.business.metadata.base.parser.metadata.spatial.c_spatialExtractorMng import CSpatialExtractorMng
 from imetadata.business.metadata.base.parser.metadata.view.c_viewCreatorMng import CViewCreatorMng
-from imetadata.database.base.c_dataset import CDataSet
 from imetadata.database.c_factory import CFactory
 
 
@@ -206,105 +205,141 @@ class CMetaDataParser(CParser):
         return md_spatial_extractor.process()
 
     def save_metadata_data_and_bus(self) -> str:
-        file_quality_text = self.metadata.quality.to_xml()
-        file_quality_summary_text = self.metadata.quality.summary()
-
-        metadata_extract_result, metadata_extract_memo, metadata_type, metadata_text = self.metadata.metadata()
-
-        # 处理业务元数据
-        metadata_bus_extract_result, metadata_bus_extract_memo, metadata_bus_type, metadata_bus_text = self.metadata.metadata_bus()
-
-        commands = [(
-            '''
-            update dm2_storage_object
-            set dso_quality = :dso_quality
-                , dso_quality_summary = :dso_quality_summary
-            where dsoid = :dsoid
-            ''', {
-                'dso_quality': file_quality_text,
-                'dso_quality_summary': file_quality_summary_text,
-                'dsoid': self.object_id,
-            }
-        ), (
-            '''
-            update dm2_storage_object
-            set dso_metadata_result = :dso_metadata_result
-                , dsometadataparsememo = :dsometadataparsememo
-                , dsometadatatype = :dsometadatatype
-                , dsometadatatext = :dsometadatatext
-                , dsometadatajson = null
-                , dsometadataxml = null
-            where dsoid = :dsoid
-            ''', {
-                'dsoid': self.object_id,
-                'dso_metadata_result': metadata_extract_result,
-                'dsometadataparsememo': metadata_extract_memo,
-                'dsometadatatype': metadata_type,
-                'dsometadatatext': metadata_text
-            }
-        ), (
-            '''
-            update dm2_storage_object
-            set dso_metadata_bus_result = :dso_metadata_bus_result
-                , dsometadata_bus_parsememo = :dsometadata_bus_parsememo
-                , dsometadatatype_bus = :dsometadatatype_bus
-                , dsometadatatext_bus = :dsometadatatext_bus
-                , dsometadatajson_bus = null
-                , dsometadataxml_bus = null
-            where dsoid = :dsoid
-            ''', {
-                'dsoid': self.object_id,
-                'dso_metadata_bus_result': metadata_extract_result,
-                'dsometadata_bus_parsememo': metadata_bus_extract_memo,
-                'dsometadatatype_bus': metadata_bus_type,
-                'dsometadatatext_bus': metadata_bus_text
-            }
-        )]
-
-        if metadata_type == self.MetaDataFormat_XML:
-            commands.append(
-                (
+        try:
+            metadata_extract_result, metadata_extract_memo, metadata_type, metadata_text = self.metadata.metadata()
+            CFactory().give_me_db(self.file_info.db_server_id).execute(
+                '''
+                update dm2_storage_object
+                set dso_metadata_result = :dso_metadata_result
+                    , dsometadataparsememo = :dsometadataparsememo
+                    , dsometadatatype = :dsometadatatype
+                    , dsometadatatext = :dsometadatatext
+                    , dsometadatajson = null
+                    , dsometadataxml = null
+                where dsoid = :dsoid
+                ''',
+                {
+                    'dsoid': self.object_id,
+                    'dso_metadata_result': metadata_extract_result,
+                    'dsometadataparsememo': metadata_extract_memo,
+                    'dsometadatatype': metadata_type,
+                    'dsometadatatext': metadata_text
+                }
+            )
+            if metadata_type == self.MetaDataFormat_XML:
+                CFactory().give_me_db(self.file_info.db_server_id).execute(
                     '''
                     update dm2_storage_object
                     set dsometadataxml = dsometadatatext::xml
                     where dsoid = :dsoid
                     ''', {'dsoid': self.object_id}
                 )
-            )
-        elif metadata_type == self.MetaDataFormat_Json:
-            commands.append(
-                (
+            elif metadata_type == self.MetaDataFormat_Json:
+                CFactory().give_me_db(self.file_info.db_server_id).execute(
                     '''
                     update dm2_storage_object
                     set dsometadatajson = dsometadatatext::json
                     where dsoid = :dsoid
                     ''', {'dsoid': self.object_id}
                 )
+        except Exception as error:
+            CFactory().give_me_db(self.file_info.db_server_id).execute(
+                '''
+                update dm2_storage_object
+                set dso_metadata_result = :dso_metadata_result
+                    , dsometadataparsememo = :dsometadataparsememo
+                    , dsometadatatype = null
+                    , dsometadatatext = null
+                    , dsometadatajson = null
+                    , dsometadataxml = null
+                where dsoid = :dsoid
+                ''',
+                {
+                    'dsoid': self.object_id,
+                    'dso_metadata_result': self.DB_False,
+                    'dsometadataparsememo': '数据元数据解析成功, 但入库过程出现异常! 元数据文本为[{0}], 详细错误信息为: [{1}]'.format(
+                        metadata_text,
+                        error.__str__())
+                }
             )
 
-        if metadata_bus_type == self.MetaDataFormat_XML:
-            commands.append(
-                (
+        # 处理业务元数据
+        try:
+            metadata_bus_extract_result, metadata_bus_extract_memo, metadata_bus_type, metadata_bus_text = self.metadata.metadata_bus()
+
+            CFactory().give_me_db(self.file_info.db_server_id).execute(
+                '''
+                update dm2_storage_object
+                set dso_metadata_bus_result = :dso_metadata_bus_result
+                    , dsometadata_bus_parsememo = :dsometadata_bus_parsememo
+                    , dsometadatatype_bus = :dsometadatatype_bus
+                    , dsometadatatext_bus = :dsometadatatext_bus
+                    , dsometadatajson_bus = null
+                    , dsometadataxml_bus = null
+                where dsoid = :dsoid
+                ''', {
+                    'dsoid': self.object_id,
+                    'dso_metadata_bus_result': metadata_extract_result,
+                    'dsometadata_bus_parsememo': metadata_bus_extract_memo,
+                    'dsometadatatype_bus': metadata_bus_type,
+                    'dsometadatatext_bus': metadata_bus_text
+                }
+            )
+
+            if metadata_bus_type == self.MetaDataFormat_XML:
+                CFactory().give_me_db(self.file_info.db_server_id).execute(
                     '''
                     update dm2_storage_object
                     set dsometadataxml_bus = dsometadatatext_bus::xml
                     where dsoid = :dsoid
                     ''', {'dsoid': self.object_id}
                 )
-            )
-        elif metadata_bus_type == self.MetaDataFormat_Json:
-            commands.append(
-                (
+            elif metadata_bus_type == self.MetaDataFormat_Json:
+                CFactory().give_me_db(self.file_info.db_server_id).execute(
                     '''
                     update dm2_storage_object
                     set dsometadatajson_bus = dsometadatatext_bus::json
                     where dsoid = :dsoid
                     ''', {'dsoid': self.object_id}
                 )
+        except Exception as error:
+            CFactory().give_me_db(self.file_info.db_server_id).execute(
+                '''
+                update dm2_storage_object
+                set dso_metadata_bus_result = :dso_metadata_bus_result
+                    , dsometadata_bus_parsememo = :dsometadata_bus_parsememo
+                    , dsometadatatype_bus = null
+                    , dsometadatatext_bus = null
+                    , dsometadatajson_bus = null
+                    , dsometadataxml_bus = null
+                where dsoid = :dsoid
+                ''', {
+                    'dsoid': self.object_id,
+                    'dso_metadata_bus_result': self.DB_False,
+                    'dsometadata_bus_parsememo': '数据业务元数据解析成功, 但入库过程出现异常! 元数据文本为[{0}], 详细错误信息为: [{1}]'.format(
+                        metadata_bus_text,
+                        error.__str__())
+                }
             )
-        # 所有元数据入库
-        CFactory().give_me_db(self.file_info.db_server_id).execute_batch(commands)
-        return CResult.merge_result(self.Success, '元数据和业务元数据处理完毕!')
+        try:
+            file_quality_text = self.metadata.quality.to_xml()
+            file_quality_summary_text = self.metadata.quality.summary()
+            CFactory().give_me_db(self.file_info.db_server_id).execute(
+                '''
+                update dm2_storage_object
+                set dso_quality = :dso_quality
+                    , dso_quality_summary = :dso_quality_summary
+                where dsoid = :dsoid
+                ''',
+                {
+                    'dso_quality': file_quality_text,
+                    'dso_quality_summary': file_quality_summary_text,
+                    'dsoid': self.object_id,
+                }
+            )
+            return CResult.merge_result(self.Success, '元数据和业务元数据处理完毕!')
+        except Exception as error:
+            return CResult.merge_result(self.Failure, '元数据和业务元数据处理完毕, 但质检结果入库时发生异常, 错误信息为: [{0}]'.format(error.__str__()))
 
     def save_metadata_time(self) -> str:
         """
@@ -315,7 +350,7 @@ class CMetaDataParser(CParser):
         if mdt_ext_result == self.DB_False:
             mdt_ext_content = None
         if CUtils.equal_ignore_case(mdt_ext_result, ''):
-            mdt_ext_content = None   # None相当于sql中的null，可以插入数据库中，而''不能插入jsonb字段中
+            mdt_ext_content = None  # None相当于sql中的null，可以插入数据库中，而''不能插入jsonb字段中
 
         # 所有元数据入库
         CFactory().give_me_db(self.file_info.db_server_id).execute(
