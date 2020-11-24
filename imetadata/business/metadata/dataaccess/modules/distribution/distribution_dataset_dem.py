b251943bcbc9a484ec4228c5ac69095bbdf876da
wxq <18437918096@163.COM>
2020-11-24 18:30:13 +0800

    修正一个调整错误

--- imetadata/business/metadata/dataaccess/modules/distribution/distribution_dataset_dem.py
+++ imetadata/business/metadata/dataaccess/modules/distribution/distribution_dataset_dem.py
@@ -47,9 +47,6 @@ class distribution_dataset_dem(distribution_guotu_dataset):
         self.add_value_to_sync_dict_list(
             sync_dict_list, 'datatype', object_table_data.value_by_name(0, 'dsodatatype', ''))
         self.add_value_to_sync_dict_list(
-            sync_dict_list, 'publishDate',
-            CUtils.to_day_format(dsometadataxml_xml.get_element_text_by_xpath_one('/root/Date'), ''))
-        self.add_value_to_sync_dict_list(
             sync_dict_list, 'sensors',
             dsometadataxml_xml.get_element_text_by_xpath_one('/root/MajorSource'))
         self.add_value_to_sync_dict_list(sync_dict_list, 'dsometadatajson', dsometadataxml)
