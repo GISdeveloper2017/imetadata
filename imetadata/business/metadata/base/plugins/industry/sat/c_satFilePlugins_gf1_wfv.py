72a0f325167b509cb066b69ccf64d22e5a9378ce
wxq <18437918096@163.COM>
2021-01-07 10:14:11 +0800

    运算bug修正

--- imetadata/business/metadata/base/plugins/industry/sat/c_satFilePlugins_gf1_wfv.py
+++ imetadata/business/metadata/base/plugins/industry/sat/c_satFilePlugins_gf1_wfv.py
@@ -218,8 +218,9 @@ class CSatFilePlugins_gf1_wfv(CSatPlugins):
         #  一定要写这个，不然部分默认的处理会不生效
         super().process_custom(metadata_bus_dict)
 
-        resolution = CUtils.dict_value_by_name(metadata_bus_dict, 'resolution', None)
-        metadata_bus_dict['resolution'] = 2 / CUtils.to_decimal(resolution)
+        resolution = CUtils.to_decimal(CUtils.dict_value_by_name(metadata_bus_dict, 'resolution', None), 0)
+        if resolution > 0:
+            metadata_bus_dict['resolution'] = 2 / resolution
 
     def get_metadata_bus_filename_by_file(self) -> str:
         """
