1045a52c532a091251a6ae297dbe927ff537e1e9
wxq <18437918096@163.COM>
2021-01-21 17:43:32 +0800

    更新遥感卫星插件

---
+++ test/metadata/inbound/plugins/test_plugins_4023_yg_ccd_file.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/4 09:05
+# @Author : 王西亚
+# @File : test_plugins_aaa.py
+import allure
+import pytest
+from imetadata.business.metadata.base.fileinfo.c_dmFilePathInfoEx import CDMFilePathInfoEx
+from imetadata.business.metadata.base.plugins.c_plugins import CPlugins
+from imetadata.business.metadata.inbound.plugins.file.plugins_4023_yg_ccd import plugins_4023_yg_ccd
+from test.metadata.inbound.plugins.plugins_test_base import Plugins_Test_Base
+
+
+@allure.feature("YG星CCD传感器压缩包文件")  # 模块标题
+class Test_plugins_4023_yg_ccd_file(Plugins_Test_Base):
+    def create_plugins(self, file_info: CDMFilePathInfoEx = None) -> CPlugins:
+        return plugins_4023_yg_ccd(file_info)
+
+    def test_file_info_list(self):
+        return [
+            {
+                self.Name_Test_File_Type: self.FileType_File,
+                self.Name_Test_file_path: 'YG24-1_CCD-A_000289219_001_009_002_L1.tar.gz',
+                self.Name_Test_object_confirm: self.Object_Confirm_IKnown,
+                self.Name_Test_object_name: 'YG24-1_CCD-A_000289219_001_009_002_L1'
+            }
+        ]
+
+
+if __name__ == '__main__':
+    pytest.main()
